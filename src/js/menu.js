ce602a939a2203cde46c3b4df690b65ca41c9ff3
THALYSON RAFAEL <rafinha.head@gmail.com>
2024-10-10 18:47:53 -0300

    commit inicial, navbar concluida

---
+++ src/js/menu.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const menuBtn = document.getElementById('menu-btn');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+  });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Inclui todos os arquivos HTML e JS dentro de src
+    "!./node_modules/**",    // Exclui a pasta node_modules
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+    },
+    extend: {},              // Pronto para futuras customizações
+  },
+  plugins: [],               // Adicione plugins aqui, se necessário
+}
