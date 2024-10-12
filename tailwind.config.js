e9f34cbed52b9952e8a6efcbeeb3bb2316b0f921
THALYSON RAFAEL <rafinha.head@gmail.com>
2024-10-12 15:02:37 -0300

    section serviços criada e parcialemnte

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}", // Inclui todos os arquivos HTML e JS dentro de src
+    "./**/*.{html,js}", // Inclui todos os arquivos HTML e JS dentro de src
     "!./node_modules/**",    // Exclui a pasta node_modules
   ],
   theme: {
